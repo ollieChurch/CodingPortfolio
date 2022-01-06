5b6f214c138b5e965530bb098f04f8e1ed6ee52c
Squeak-Paradox <olliemchurch@gmail.com>
2022-01-06 14:45:38 +0000

    update about me text and replaces img with headshot photo

--- src/pages/Home/MoreInfo/AboutMe/index.js
+++ src/pages/Home/MoreInfo/AboutMe/index.js
@@ -1,17 +1,19 @@
-import UnboxingDoodle from '../../../../assets/images/svg/UnboxingDoodle'
+import profileImg from '../../../../assets/images/jpg/profileImg.jpg'
 import './aboutMe.css'
 
 function AboutMe() {
     return (
         <div className='aboutMe'>
-            <h3 className='moreInfo-title'>
-                About Me <i className="far fa-grin-alt"></i>
-            </h3>
-            
-            <p className='aboutMe-text dropFont'>
-                A joy of puzzles, a generous creative drive, & a large splash of logical reasoning. My (sometimes mad) multi-faceted work life has required a high level of organisation and the ability to compartmentalise workloads while allowing relevant skills and contacts to overlap. As a self-taught frontend developer I am looking forward to bringing the left and right sides of my brain together as I focus in on an exciting career.
-            </p>
-            <UnboxingDoodle />
+            <div>
+                <h3 className='moreInfo-title'>
+                    About Me <i className="far fa-grin-alt"></i>
+                </h3>
+                
+                <p className='aboutMe-text dropFont'>
+                    A joy of puzzles, a generous creative drive, & a large splash of logical reasoning. My (sometimes mad) multi-faceted work life has required a high level of organisation and the ability to compartmentalise workloads while allowing relevant skills and contacts to overlap. As a self-taught frontend developer I enjoy creating interactive content with intuitive UI and using creative problem solving to fulfil a brief.
+                </p>
+            </div>
+            <img src={profileImg} alt='Ollie Church' className='aboutMe-img'/>
         </div>
     )
 }
