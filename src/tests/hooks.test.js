cc30599c195367de2ffaed59677e7be9d4a790af
Squeak-Paradox <olliemchurch@gmail.com>
2022-01-10 23:16:10 +0000

    refactors tests

--- src/tests/hooks.test.js
+++ src/tests/hooks.test.js
@@ -1,17 +1,23 @@
-import useResetScrollTest from './hooks/useResetScrollTest'
-import useWindowSizeTest from './hooks/useWindowSizeTest'
+import { renderHook } from '@testing-library/react-hooks'
+import useResetScroll from '../hooks/useResetScroll';
+import useWindowSize from '../hooks/useWindowSize'
 
 window.scrollTo = jest.fn()
-
-afterEach(() => {
-  jest.resetAllMocks()
-})
-
-afterAll(() => {
-  jest.clearAllMocks()
-})
+afterEach(() => { jest.resetAllMocks() })
+afterAll(() => { jest.clearAllMocks() })
 
 describe('hooks functionality', () => {
-  useResetScrollTest()
-  useWindowSizeTest()
+  test('useResetScroll calls window.scrollTo', () => {
+    jest.spyOn(window, 'scrollTo')
+    renderHook(() => useResetScroll())
+    expect(window.scrollTo).toBeCalled()
+  })
+
+  test('useWindowSize returns window height & width', () => {
+    const { result } = renderHook(() => useWindowSize())
+    expect(result.current.windowSize).toEqual({
+      width: window.innerWidth, 
+      height: window.innerHeight
+    })
+  })
 })
