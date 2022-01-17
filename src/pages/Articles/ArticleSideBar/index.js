eb64ef1da6ce851a875696c9d57e72005a40498a
Squeak-Paradox <olliemchurch@gmail.com>
2022-01-17 13:12:40 +0000

    creates articles tags sidebar

--- src/pages/Articles/ArticleSideBar/index.js
+++ src/pages/Articles/ArticleSideBar/index.js
@@ -4,11 +4,20 @@ import FeaturedCard from '../../../components/FeaturedCard'
 
 function ArticleSideBar() {
     return (
-        <aside>
-            <LightPanel addedClasses='articles-sideBar'>
-                <h3 className='featuredArticles-head'>Featured Articles <i className="far fa-newspaper"></i></h3>
+        <aside className='articles-sideBar'>
+            <LightPanel addedClasses='articles-sideBar-panel sideBar-featured'>
+                <h3 className='articles-sideBar-head'>Featured Articles <i className="far fa-newspaper"></i></h3>
                 <FeaturedCard topic='codingjourney'/>
                 <FeaturedCard topic='lovecssgrid'/>
+            </LightPanel>
+            <LightPanel addedClasses='articles-sideBar-panel'>
+                <h3 className='articles-sideBar-head'>Article Tags <i class="fas fa-tags"></i></h3>
+                <div className='articles-tagsContainer'>
+                    <button>tag1</button>
+                    <button>tag2</button>
+                    <button>tag3</button>
+                    <button>tag4</button>
+                </div>
             </LightPanel>
         </aside>
     )
