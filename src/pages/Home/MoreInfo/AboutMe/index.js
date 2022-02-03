14a6f4a6e21b550b7ad0765fa49d36e88bb56424
Squeak-Paradox <olliemchurch@gmail.com>
2022-02-03 10:53:29 +0000

    fixes typo

--- src/pages/Home/MoreInfo/AboutMe/index.js
+++ src/pages/Home/MoreInfo/AboutMe/index.js
@@ -12,7 +12,7 @@ function AboutMe() {
                 <div className='aboutMe-text dropFont'>
                     <p>Puzzle addict turned coding enthusiast. 4 years experience managing a small team with a focus on 5 star customer service for over 31000 happy players.</p>
                     
-                    <p>There are 3 top tips for completing an escape room; search, speak, simple. It turns out thatthey apply to my work and coding too. Find the relevant tools and knowledge, communicate withthe team, look for the simplest solution.</p>
+                    <p>There are 3 top tips for completing an escape room; search, speak, simple. It turns out that they apply to my work and coding too. Find the relevant tools and knowledge, communicate withthe team, look for the simplest solution.</p>
                     
                     <p>Having truly fallen in love with frontend development during the 2020/21 lockdowns, I am looking to make my career change and take the next step in an entry level role.</p>
                 </div>
